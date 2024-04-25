7442a4f990c1f76f55f5cca1342a9c2ff3ebbbeb
Aaron Sumner <asumner@mac.com>
2024-04-24 21:17:34 -0500

    Add recipes scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_020637) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_021439) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id", null: false
+    t.text "description"
+    t.text "ingredients"
+    t.text "instructions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_recipes_on_category_id"
+  end
+
+  add_foreign_key "recipes", "categories"
 end
