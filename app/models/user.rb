aa3a203e7b8e4a51d555990655205e3f9af655cc
Aaron Sumner <asumner@mac.com>
2024-05-15 17:57:27 -0500

    Save avatar to user on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class User < ApplicationRecord
   include Clearance::User
 
@@ -5,11 +7,13 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_recipes, through: :favorites, source: :recipe
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
 
   validates :nickname, presence: true, uniqueness: true
   validates :api_token, presence: true, uniqueness: true
 
   before_validation :set_api_token, on: :create
+  before_create :set_avatar
 
   private
 
@@ -17,5 +21,10 @@ class User < ApplicationRecord
     begin
       self.api_token = SecureRandom.uuid
     end while self.class.find_by(api_token: api_token).present?
+  end
+
+  def set_avatar
+    download = URI.open("https://api.dicebear.com/8.x/thumbs/png?seed=#{nickname}")
+    self.avatar.attach(io: download, filename: "avatar.png", content_type: "image/png")
   end
 end
