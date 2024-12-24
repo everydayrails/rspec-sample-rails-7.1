e743beeaab86434a5f19c8917a3660edcf717cf9
Aaron Sumner <asumner@mac.com>
2024-12-23 18:32:09 -0600

    Fill in missing recipe factory data

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :recipe do
     sequence(:name) { |n| "Delicious recipe #{n}" }
     description { "A great-tasting dish!" }
+    ingredients { "1 cup of sugar" }
+    instructions { "Mix the ingredients together" }
     association :category
     association :user
   end
